29794e84ed8625b03d19523bfd79a73b1c0012a3
eliappo <237402327+eliappo@users.noreply.github.com>
2026-02-07 13:07:21 +0100

    First draft of go development container

---
+++ develop.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ "$1" = "build" ]; then
+    if [ -f "./docker/dev/Dockerfile" ]; then
+        echo "[INFO] Building docker image..."
+        docker build --build-arg GO_VERSION=1.25.0 -t ubuntu-go ./docker/dev/
+        exit 0
+    fi
+elif [ "$1" = "run" ]; then 
+    if docker images | grep -o ubuntu-go; then
+        echo "[INFO] Running ubuntu go developement environment"
+        docker run -it -v ./src:/go/src ubuntu-go  
+        exit 0
+    else
+        echo "[ERROR] Image ubuntu-go not found. Run '$0 build' first"
+        exit 1
+    fi
+else
+    echo "Usage: $0 {build|run}"
+    exit 1
+fi
+
---
+++ src/go.mod
@@ -0,0 +1,3 @@
+module minitwit
+
+go 1.25.0
