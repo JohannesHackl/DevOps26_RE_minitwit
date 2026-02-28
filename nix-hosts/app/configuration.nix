a55b6cdc9813149e34ff012f88f46924ed53dda6
eliappo <237402327+eliappo@users.noreply.github.com>
2026-02-28 19:13:12 +0100

    fix: adding permission to root login in vm

--- nix-hosts/app/configuration.nix
+++ nix-hosts/app/configuration.nix
@@ -10,12 +10,16 @@
 
   networking.hostName = "minitwit-app";
 
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings.PermitRootLogin = "yes";
+  };
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJrmlWbyrXyqEI8nP/N31d1yfT314rk3Jr7DS47f6Q27 desktop ssh"
   ];
 
   users.users.root.password = "123";
+
   virtualisation.vmVariant = {
     virtualisation.forwardPorts = [
       { from = "host"; host.port = 2222; guest.port = 22; }
