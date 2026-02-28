4e072c74baab7755946146ac2612f126a1944b96
eliappo <237402327+eliappo@users.noreply.github.com>
2026-02-28 20:08:28 +0100

    Refactor: Getting minitwit directories from envirement variables

--- modules/minitwit-app.nix
+++ modules/minitwit-app.nix
@@ -3,8 +3,13 @@ let
   minitwit = pkgs.buildGoModule {
     pname = "minitwit";
     version = "latest";
-    src = ./.;
-    vendorHash = null;
+    src = ../.;
+    vendorHash = "sha256-zVj7biULqStZsrAe3xLNkOOX3ol/RLMUitmd2YujSLM=";
+    ###    postInstall = ''
+    ###      mkdir -p $out/share/minitwit
+    ###      cp -r templates $out/share/minitwit/templates
+    ###      cp -r static $out/share/minitwit/static
+    ###    '';
   };
 in
 {
@@ -22,20 +27,21 @@ in
       after = [ "network.target" ];
       environment = {
         DB_ADDR = config.services.minitwit.dbAddr;
+        TEMPLATES_PATH = "${minitwit}/share/minitwit/templates";
+        STATIC_PATH = "${minitwit}/share/minitwit/static";
       };
       serviceConfig = {
-        ExecStart = "${minitwit}/bin/minitwit";
-        WorkingDirectory = "/var/lib/minitwit";
+        ExecStart = "${minitwit}/bin/RE_minitwit";
         Restart = "always";
         User = "minitwit";
       };
     };
 
-    systemd.tmpfiles.rules = [
-      "d /var/lib/minitwit 0750 minitwit minitwit -"
-      "L /var/lib/minitwit/templates - - - - ${minitwit}/share/minitwit/templates"
-      "L /var/lib/minitwit/static - - - - ${minitwit}/share/minitwit/static"
-    ];
+    ###    systemd.tmpfiles.rules = [
+    ###      "d /var/lib/minitwit 0750 minitwit minitwit -"
+    ###      "L /var/lib/minitwit/templates - - - - ${minitwit}/share/minitwit/templates"
+    ###      "L /var/lib/minitwit/static - - - - ${minitwit}/share/minitwit/static"
+    ###    ];
 
     users.users.minitwit = {
       isSystemUser = true;
